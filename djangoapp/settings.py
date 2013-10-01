51327166b122a5c52f97d7186f3eb00c24406994
Vishwas Sharma <vishwasinventor@gmail.com>
2013-10-01 22:01:53 +0530

    First app which is working is being pushed

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -10,17 +10,53 @@ DATABASES['native'] = DATABASES['default']
 DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
 AUTOLOAD_SITECONF = 'indexes'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Name of the directory for the project.
+PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = STATIC_URL + "media/"
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+
+FILE_UPLOAD_PERMISSIONS = 0644
+
+
 INSTALLED_APPS = (
-#     'django.contrib.admin',
-    'django.contrib.contenttypes',
+    'suit',
+    'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.redirects',
     'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'django.contrib.messages',
     'djangotoolbox',
     'autoload',
     'dbindexer',
-
+    'django.contrib.staticfiles',
+    'mysite',
+#     'django.contrib.sites',    # for serving flatpages   
+#     'django.contrib.flatpages',      # flatpages
+#     'django.contrib.sitemaps', # For making sitemaps dyanmically
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
 )
@@ -28,23 +64,38 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     # This loads the index definitions, so it has to come first
     'autoload.middleware.AutoloadMiddleware',
-
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.request',
-    'django.core.context_processors.media',
+    "django.contrib.auth.context_processors.auth",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.static",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.core.context_processors.tz",
 )
+
+# TEMPLATE_LOADERS = (
+#     'django.template.loaders.app_directories.Loader',
+# )
 
 # This test runner captures stdout and associates tracebacks with their
 # corresponding output. Helps a lot with print-debugging.
 TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
 
 ADMIN_MEDIA_PREFIX = '/media/admin/'
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
+)
 
 ROOT_URLCONF = 'urls'
+
+SUIT_CONFIG = {
+    'ADMIN_NAME': 'MeraVote.in',
+}
